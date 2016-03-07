3f4a70e516c30bf49504a56a2054e6484a33c236
Eugene Ragasa <eragasa@ufl.edu>
2016-03-07 17:39:22 -0500

    updated buckingham
    added buckingham potential

---
+++ examples/read_eam_file/compare_eam_alloy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 17:37:47 2016
+
+@author: Eugene
+"""
+
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyflamestk',
+      version='0.1',
+      description='FLAMES toolkit for the simulation of materials',
+      url='https://sites.google.com/site/notesinmaterialsscience/pyflamestk',
+      author='Eugene Ragasa',
+      author_email='eragasa@ufl.edu')
