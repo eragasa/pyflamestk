4a8af0edaa74301c76c1ee6b4b02a5162931ca6a
eragasa <eragasa@ufl.edu>
2017-03-04 16:42:40 -0500

    base.py

--- pyflamestk/base.py
+++ pyflamestk/base.py
@@ -1,15 +1,30 @@
-import copy
-import subprocess
+# -*- coding: utf-8 -*-
+"""This module provides the base classes for pyflamestk."""
+__author__ = "Eugene J. Ragasa"
+__copyright__ = "Copyright (C) 2016,2017"
+__license__ = "Simplified BSD License"
+__version__ = "1.0"
+
+import copy, subprocess
 import numpy as np
-    
-class Atom:
-   def __init__(self, symbol, position):
+
+class Atom(object):
+    """description of an atom"""
+   def __init__(self, symbol, position, magmom = 0):
+       """__init__ method
+       Args:
+           symbol (str): the standard ISO symbol for an element
+           position (list of float): the position of the atom the units
+              are dependent upon use.
+           magmom (float): the magnetic moment of the atom
+       """
        self._symbol = symbol
        self._position = position
-       self._magnetic_moment = 0
+       self._magnetic_moment = magmom
        
    @property
-   def symbol(self): 
+   def symbol(self):
+       """str: the standard ISO symbol for an element"""
        return self._symbol
        
    @symbol.setter
@@ -17,7 +32,8 @@ class Atom:
        self._symbol = s
        
    @property
-   def position(self): 
+   def position(self):
+       """list of float: the position of the atom."""
        return np.array(self._position)
        
    @position.setter
@@ -26,16 +42,33 @@ class Atom:
            
    @property
    def magnetic_moment(self): 
+       """float: the magnetic moment of the atom."""
        return self._magnetic_moment
        
    @magnetic_moment.setter
    def magnetic_moment(self,m): 
        self._magnetic_moment = m
 
-class Structure:
-    
+class Structure(object):
+    """A structural representation of a material system
+   
+    Note:
+        The position of the atom must be the same as the basis vectors 
+        defined by the H-matrix.
+
+    Todo:
+        This module is only written to deal with cells with orthogonal unit vectors.  
+
+    """ 
     def __init__(self, obj=None):
+        """__init__
+
+        Args:
+            obj (pyflamestk.base.Structure,optional): if this argument is set then the
+               this constructor acts as a copy constructor. 
+        """
         if obj is None:
+            assert type(object),pyflamestk.base.Structure
             self._noncopy_init()
         else:
             self._copy_init(obj)
@@ -45,7 +78,7 @@ class Structure:
             
     def _noncopy_init(self):
         self._atoms = []
-        self._structure_comment = None
+        self._structure_comment = ""
         self._lattice_parameter = 1.0
         self._h_matrix = np.zeros(shape=[3,3])
         
@@ -58,6 +91,7 @@ class Structure:
 
     @property
     def a1(self):
+        """float: the length of h1 lattice vector"""
         a0 = self.lattice_parameter
         h1 = self.h1
         a1 = a0 * h1.dot(h1)**0.5
@@ -65,6 +99,7 @@ class Structure:
 
     @property
     def a2(self):
+        """float: the length of the h2 lattice vector"""
         a0 = self.lattice_parameter
         h2 = self.h2
         a2 = a0 * h2.dot(h2)**0.5
@@ -72,13 +107,15 @@ class Structure:
 
     @property
     def a3(self):
+        """float: the length of the h3 lattice vector"""
         a0 = self.lattice_parameter
         h3 = self.h3
         a3 = a0*h3.dot(h3)**0.5
         return a3
 
     @property
-    def lattice_parameter(self): 
+    def lattice_parameter(self):
+        """float: the length of the lattice parameter, usually in Angstroms."""
         return self._lattice_parameter
        
     @lattice_parameter.setter
@@ -90,6 +127,7 @@ class Structure:
 
     @property
     def structure_comment(self): 
+        """str: a comment about the structure"""
         return self._structure_comment
        
     @structure_comment.setter
@@ -98,6 +136,7 @@ class Structure:
 
     @property
     def h_matrix(self):
+        """numpy.array: this is a 3x3 numpy array"""
         return self._h_matrix
 
     @h_matrix.setter
@@ -106,6 +145,7 @@ class Structure:
 
     @property
     def h1(self):
+        """numpy.array: this is a 3x1 numpy array"""
         return np.array(self._h_matrix[0,:])
 
     @h1.setter
@@ -114,6 +154,7 @@ class Structure:
 
     @property
     def h2(self):
+        """numpy.array: this is a 3x1 numpy array"""
         return np.array(self._h_matrix[1,:])
 
     @h2.setter
@@ -122,6 +163,7 @@ class Structure:
  
     @property
     def h3(self):
+        """numpy.array: this is a 3x1 numpy array"""
         return np.array(self._h_matrix[2,:])
 
     @h3.setter
@@ -130,6 +172,7 @@ class Structure:
 
     @property
     def b1(self):
+        """numpy.array: this is a 3x1 numpy array, in reciprocal space"""
         a1 = np.array(self.h1)
         a2 = np.array(self.h2)
         a3 = np.array(self.h3)
@@ -139,6 +182,7 @@ class Structure:
 
     @property
     def b2(self):
+        """numpy.array: this is a 3x1 numpy array, in reciprocal space"""
         a1 = np.array(self.h1)
         a2 = np.array(self.h2)
         a3 = np.array(self.h3)
@@ -149,6 +193,7 @@ class Structure:
 
     @property
     def b3(self):
+        """numpy.array: this is a 3x1 numpy array, in reciprocal space"""
         a1 = np.array(self.h1)
         a2 = np.array(self.h2)
         a3 = np.array(self.h3)
@@ -158,26 +203,33 @@ class Structure:
         
     @property
     def n_atoms(self):
-        return(len(self._atoms))
+        """float: the number of atoms in the structure"""
+        n_atoms = len(self._atoms)
+
+        return n_atoms
 
     @property
     def symbols(self):
+        """list of str: a list of the symbols in the structure"""
         symbols = []
         for a in self._atoms:
             if not(a.symbol in symbols):
                 symbols.append(a.symbol)
-        return list(symbols)
+        return symbols
 
     @property
     def atoms(self):
+        """list of pyflamestk.base.Atom: a list of the atoms in the structure"""
         return self._atoms
         
     @atoms.setter
     def atoms(self, atoms):
+        # this does a deep copy of the atoms
         self._atoms  = copy.deepcopy(atoms)
 
     @property
     def vacancies(self):
+        """list of pyflamestk.base.Atom: a list of vacancies in the structure"""
         return self._vacancies
 
     @property
@@ -199,6 +251,19 @@ class Structure:
         return is_atom_exists # = False
 
     def add_atom(self, symbol, position):
+        """add an atom to the structure
+
+        Checks to see if an existing atom exists at the position we are trying
+        to add an atom, then if the position is empty.  The atom is added then
+        added to the list of interstitials.
+
+        Args:
+            symbol (str): the symbol of the atom to be added
+            position (str): the position of the atom to be added
+
+        Raises:
+            ValueError: If an atom already exists in the position.
+        """
         is_atom_exists = self.check_if_atom_exists_at_position(symbol,position)
         if is_atom_exists is True:
             err_msg = "Tried to add {} @ {} an atom already there"
@@ -209,6 +274,16 @@ class Structure:
             self._interstitials.append([symbol,position])
 
     def remove_atom(self,symbol, position):
+        """ remove an atom from the structure
+
+        This method checks for atom at the position, if an atom exists.  It is
+        removed from the structure then the position is recorded as a vacancy.
+
+        Args:
+            symbol (str): the symbol of the atom
+            position (list of float): the position of the atom
+
+        """
         for i,a in enumerate(self._atoms):
             if (a.symbol == symbol):
                 diff = [abs(position[j]-a.position[j]) for j in range(3)]
@@ -268,6 +343,15 @@ class Structure:
                                     a.magnetic_moment)
  
 def make_super_cell(structure, sc):
+    """makes a supercell from a given cell
+
+    Args:
+        structure (pyflamestk.base.Structure): the base structure from which
+            the supercell will be made from.
+        sc (list of int): the number of repeat units in the h1, h2, and h3 
+            directions
+    """
+
     supercell = Structure()
     supercell.structure_comment = "{}x{}x{}".format(sc[0],sc[1],sc[2])
 
@@ -295,14 +379,27 @@ def make_super_cell(structure, sc):
     # return a copy of the supercell
     return copy.deepcopy(supercell)
 
-class Simulation:
+class Simulation(object):
+    """ an abstract class for simulations
+
+    not currently using this yet
+    """
     def __init__(self):
         raise NotImplementedError
     def run(self):
         raise NotImplementedError
 
-# assumes a unix-like operating system
 def tail(fname, n_lines, offset=None):
+    """ replicates the tail command from unix like operations systems
+    
+    Args:
+        fname (str): filename
+        n_lines (int): the number of lines
+
+    Note:
+        this is dependent upon the tail command in the unx operating system
+        this should actually be rewritten as to be OS agnostic.
+    """
     cmd_str = "/usr/bin/tail -n {} {}".format(str(n_lines), fname)
     p = subprocess.Popen(cmd_str, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
