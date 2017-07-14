df0fee87ff90b61aebbf993fec84d6a1e4353135
eragasa <eragasa@ufl.edu>
2017-07-14 17:08:01 -0400

    fixed wrong path for LAMMPS_BIN

--- examples/lmps_MgO_pareto/runsimulation_flames.sh
+++ examples/lmps_MgO_pareto/runsimulation_flames.sh
@@ -5,7 +5,7 @@ echo The PYTHONPATH is $PYTHONPATH
 
 # this is required for FLAMES clusters
 # LAMMPS_BIN must point to a valid serial version of LAMMPS
-export LAMMPS_BIN=$(~/;pwd)/bin/lmp_serial
+export LAMMPS_BIN=$HOME/bin/lmp_serial
 
 bash cleanup.sh
 python buckingham_pareto.py 
