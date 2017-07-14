a2c0beef06f5dff0bf61193d270a90647c016fed
eragasa <eragasa@ufl.edu>
2017-07-14 16:26:12 -0400

    fixed lmps_MgO_pareto for pratyusha

--- examples/lmps_MgO_pareto/runsimulation.sh
+++ examples/lmps_MgO_pareto/runsimulation.sh
@@ -2,4 +2,11 @@
 
 export PYTHONPATH=$(cd ../../;pwd)
 echo The PYTHONPATH is $PYTHONPATH
+
+# tmux appears to be broken due to the brew install process on OSX
+# this produces the error:
+#     [warn] kq_init: detected broken kqueue; not using.: No such file or directory
+# this comment suppresses the error
+export EVENT_NOKQUEUE=1
+
 python buckingham_pareto.py 
