a03f8eea32af487f30e50d383f589286ac51da95
eragasa <eragasa@ufl.edu>
2017-07-25 15:13:03 -0400

    fixed OSX kqueue error in simulation script

--- examples/MgO_buckingham/lmps_MgO_serial_iterate/runsimulation_osx.sh
+++ examples/MgO_buckingham/lmps_MgO_serial_iterate/runsimulation_osx.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-PYTHONPATH=$(cd ../../;pwd)
+PYTHONPATH=$(cd ../../../;pwd)
+echo The PYTHONPATH is $PYTHONPATH
 
+# tmux appears to be broken due to the brew install process on OSX
+# this produces the error:
+#     [warn] kq_init: detected broken kqueue; not using.: No such file or directory
+# this comment suppresses the error
+export EVENT_NOKQUEUE=1
+
+bash cleanup.sh
 python buckingham_iterate.py
