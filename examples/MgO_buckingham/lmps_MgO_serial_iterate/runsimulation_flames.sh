7060d337bab75b3c8acfff6e0df6b8b0a3f363fb
Sai Prathyusha Yadama <prathyusha@demeter.mse.ufl.edu>
2017-07-25 15:37:35 -0400

    changed the path of the Lammps executable

--- examples/MgO_buckingham/lmps_MgO_serial_iterate/runsimulation_flames.sh
+++ examples/MgO_buckingham/lmps_MgO_serial_iterate/runsimulation_flames.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export LAMMPS_BIN=$(~/;)/bin/lmp_serial
+export LAMMPS_BIN=$(cd ~/;pwd)/bin/lmp_serial
+echo The LAMMPS_BIN is $LAMMPS_BIN
 export PYTHONPATH=$(cd ../../../;pwd)
 echo The PYTHONPATH is $PYTHONPATH
 
