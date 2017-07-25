2588b5c55b29b29ff6ef74656ecfcaa54b581291
eragasa <eragasa@ufl.edu>
2017-07-25 15:27:09 -0400

    added export of LAMMPS_BIN for FLAMES clsuters

--- examples/MgO_buckingham/lmps_MgO_serial_iterate/runsimulation_flames.sh
+++ examples/MgO_buckingham/lmps_MgO_serial_iterate/runsimulation_flames.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
+export LAMMPS_BIN=$(~/;)/bin/lmp_serial
 export PYTHONPATH=$(cd ../../../;pwd)
 echo The PYTHONPATH is $PYTHONPATH
-
-# tmux appears to be broken due to the brew install process on OSX
-# this produces the error:
-#     [warn] kq_init: detected broken kqueue; not using.: No such file or directory
-# this comment suppresses the error
-export EVENT_NOKQUEUE=1
 
 bash cleanup.sh
 python buckingham_iterate.py
